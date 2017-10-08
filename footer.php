42c69f255f6d99afb652ddcb6205fc2234db7b7f
aneteser <aneteser@gmail.com>
2017-10-08 19:01:21 +0200

    footer css and php
    footer php doc added + footer css added to main.css

---
+++ footer.php
@@ -0,0 +1,88 @@
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="footer.css">
+    
+    <footer>
+     <div class="container">
+       <div class="row">
+       
+                <div class="footercol">
+                    <ul class="adress">
+                         <span>Adress</span>    
+                         <li>
+                             <p> Chlastawa 17, 66-210 Chlastawa, Guatemala</p>
+                          </li>
+                               
+                          <li>
+                            <p>+90 1234 56789</p>
+                          </li>
+                               
+                          <li>
+                            <p>info@learnit.com</p>
+                          </li>
+                     </ul>
+                </div>
+                
+                <div class="footercol">
+                    <ul class="content">
+                         <span>Content</span>    
+                         <li>
+                            <a href="#">Home</a>
+                          </li>
+                               
+                          <li>
+                             <a href="#">About</a>
+                          </li>
+                               
+                          <li>
+                            <a href="#">Blog</a>
+                          </li>
+                               
+                          <li>
+                             <a href="#">Gallery </a>
+                          </li>
+                               
+                          <li>
+                            <a href="#">Profile</a>
+                         </li>
+                         
+                    </ul>
+                </div>
+           
+              <div class="footercol">
+                   <ul class="contact">
+                              <span>Connect</span>  
+                              <form>
+                                  Name:<br/>
+                                  <input type="text" name="firstname"><br/>
+                                  Email:<br/>
+                                  <input type="email" name="email"><br/>
+                                  <input type="submit" value="Submit">
+                              </form>
+                               <li>
+                                    <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                               </li>
+                              
+                               <li>
+                                    <a href="#"><i class="fa fa-github fa-2x"></i></a>
+                               </li>
+                                
+                               <li>
+                                    <a href="#"><i class="fa fa-linkedin fa-2x"></i></a>
+                               </li>
+                               
+                               <li>
+                                    <a href="#"><i class="fa fa-tumblr fa-2x"></i></a>
+                               </li>
+                                
+                               <li>
+                                    <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                              </li>
+                              
+                     </ul>
+                </div>
+           
+           
+           </div> 
+        </div>
+    </footer>
