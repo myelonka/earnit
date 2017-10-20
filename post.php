c33f213384231a43070cc62300c9a7ddb30df148
aneteser <aneteser@gmail.com>
2017-10-20 20:16:09 +0200

    footer changes
    1

--- post.php
+++ post.php
@@ -17,19 +17,21 @@
 		?>
 		
 		<div class="page-container">
-			<div class="col-1">1</div>
-			<div class="col-1">2</div>
-			<div class="col-1">3</div>
-			<div class="col-1">4</div>
-			<div class="col-1">5</div>
-			<div class="col-1">6</div>
-			<div class="col-1">7</div>
-			<div class="col-1">8</div>
-			<div class="col-1">9</div>
 			<div class="col-1">10</div>
-			<div class="col-1">11</div>
-			<div class="col-1">12</div>
-			<div class="col-12"><h1>Post</h1></div>
+			<div class="col-12"><h1>Post a job offer</h1></div>
+            
+            <form method="post" action="formtest.php">
+                Company name:<br>
+              <input type="text" name="author"><br>
+              Title:<br>
+              <input type="text" name="title"><br>
+              Job description:<br>
+              <textarea type="text" name="description" cols="80px" rows="15" wrap="soft">
+              </textarea>
+              
+              <input type="submit">
+            </form>
+            
 			<div class="col-6"><h2>Lorem ipsum dolor sit amet.</h2></div>
 			<div class="col-6" style="grid-column: 7/13"><h2>Lorem ipsum dolor sit amet.</h2></div>
 			<div class="col-3"><h3>Lorem ipsum dolor sit amet.</h3></div>
