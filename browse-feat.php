bfbfbef61d79bd81523ebf4a9005283d2c4380d6
aneteser <aneteser@gmail.com>
2017-11-09 21:13:54 +0100

    upd

--- browse-feat.php
+++ browse-feat.php
@@ -5,7 +5,7 @@
 
 if ($db->connect_error){
     echo "could not connect: " . $db->connect_error;
-    printf("<br><a href=index.php?>Return to home page </a>");
+    printf("<br><a href=index.php>Return to home page </a>");
     exit();
 }
 
@@ -47,6 +47,17 @@ while ($stmt->fetch()) {
             echo '<p>Application deadline:<br>' .$deadline. '</p><br>';
             echo '<h2>Application form</h2><br>';
           
+            if (isset($_POST['submit_application'])){
+            $query = "INSERT IGNORE INTO usertopost (postId, userId) VALUES (?, ?)";
+            if (!$stmt = $db->prepare($query)){
+                 printf($db->error);
+            }
+            $stmt->bind_param('ii', $id, $_SESSION['login_user']);
+            if (!$stmt->execute()){
+                printf($db->error);
+                }
+            }
+
             $usersId = [];
             $query = "SELECT * FROM usertopost WHERE postId=$id";
             $stmt = $db->prepare($query);
@@ -69,25 +80,16 @@ while ($stmt->fetch()) {
             }
 
 
-            if (isset($_POST['submit_application'])){
-                $query = "INSERT IGNORE INTO usertopost (postId, userId) VALUES (4, 8)";
-                if (!$stmt1 = $db->prepare($query)){
-                    printf($db->error);
-                }
-                //$stmt->bind_param('ss', $id, $_SESSION['login_user']);
-                if (!$stmt1->execute()){
-                    printf($db->error);
-                }
-            }
+
 
             if (isset($_FILES['submit_application'])){
                 $allowedextensions = array('pdf');
                 $extension = strtolower(substr($_FILES['upload']['name'], strrpos($_FILES['upload']['name'], '.') + 1));
                 if(in_array($extension, $allowedextensions) === false){
                     #add a new array entry
-                        $error[] = '<br> This is not an PDF file, upload is allowed only for PDF files.';
+                        $error[] = 'This is not an PDF file, upload is allowed only for PDF files.';
                     if($_FILES['upload']['size'] > 1000000){
-                         $error[]='<br> The file exceeded the upload limit';
+                         $error[]='The file exceeded the upload limit';
                     }
                     if(empty($error)){    
                         move_uploaded_file($_FILES['upload']['tmp_name'], "uploads/{$_FILES['upload']['name']}");     
@@ -101,21 +103,19 @@ while ($stmt->fetch()) {
             }
         ?>
             <form id="featured_form" method="post" action="" enctype="multipart/form-data">
-              <h1> Name: </h1>
-              <input type="text" name="employee_name" class="back" value="">
-              <h1> Surname: </h1>
-              <input type="text" name="employee_surname" class="back" value="">
-              <h1> Motivation letter (max 5000 characters): </h1>
-              <textarea maxlength="5000" type="text" class="back" name="description" value="description" cols="40px" rows="15" wrap="soft"></textarea>
-              <h1> Upload your CV (PDF format): </h1>
-              <input id="fileToUpload" type="file" name="fileToUpload">
-              <br><br>
+              Name:<br>
+              <input type="text" name="employee_name" class="back" value=""><br><br>
+              Surname:<br>
+              <input type="text" name="employee_surname" class="back" value=""><br><br>
+              Motivation letter (max 5000 characters):<br>
+              <textarea maxlength="5000" type="text" class="back" name="description" value="description" cols="40px" rows="15" wrap="soft"></textarea><br><br>
+              Upload your CV (pdf format):<br>
+              <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
               <input type="submit" value="Apply" name="submit_application" class="submit_forms"><br><br>
              </form>
              <?php 
-             
             foreach ($emailsId as $key => $value) {
-                echo "<h2>".$value."</h2><br>";
+                echo "<h1>".$value."</h2><br>";
             }
              ?>
 	</div>
