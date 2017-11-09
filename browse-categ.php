98eef19f2430b53ea5ad8dc8ee74ea49718e271a
aneteser <aneteser@gmail.com>
2017-11-09 19:46:59 +0100

    vcdd

--- browse-categ.php
+++ browse-categ.php
@@ -51,7 +51,92 @@ while ($stmt->fetch()) {
     <div id="openModal" class="modalDialog">
         <div>
             <a href="#close" title="Close" class="close">X</a>
+             <?php
+            // Retrieve the id set in the url
+            $field = $_POST['field'];
+            //ask all the data from database associated to this id
+            $query = "SELECT author, title, description, employerMail, deadline FROM posts WHERE category=$field";
+            $stmt = $db->prepare($query);
+            $stmt->bind_result($author, $title, $description, $employerMail, $deadline);    
+            $stmt->execute();
+            $stmt->fetch();
+            $stmt->close();
+            echo '<h2>' . $title . '</h2><br>';
+            echo '<p>Hiring company:' .$author. '</p><br>';
+            echo '<p>Job description:<br>' .$description. '</p><br>';
+            echo '<p>Application deadline:<br>' .$deadline. '</p><br>';
+            echo '<h2>Application form</h2><br>';
+          
+            $usersId = [];
+            $query = "SELECT * FROM usertopost WHERE postId=$id";
+            $stmt = $db->prepare($query);
+            $stmt->bind_result($postId, $userId);
+            $stmt->execute();
+            while ($stmt->fetch()){
+                array_push($usersId, $userId);
+            }
+            $stmt->close();
             
+            $emailsId = [];
+            foreach ($usersId as $key => $value) {
+                $query = "SELECT email FROM users WHERE id=$value";
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($email);
+                $stmt->execute();
+                $stmt->fetch();
+                array_push($emailsId, $email);
+                $stmt->close();
+            }
+
+
+            if (isset($_POST['submit_application'])){
+                $query = "INSERT IGNORE INTO usertopost (postId, userId) VALUES (4, 8)";
+                if (!$stmt1 = $db->prepare($query)){
+                    printf($db->error);
+                }
+                //$stmt->bind_param('ss', $id, $_SESSION['login_user']);
+                if (!$stmt1->execute()){
+                    printf($db->error);
+                }
+            }
+
+            if (isset($_FILES['submit_application'])){
+                $allowedextensions = array('pdf');
+                $extension = strtolower(substr($_FILES['upload']['name'], strrpos($_FILES['upload']['name'], '.') + 1));
+                if(in_array($extension, $allowedextensions) === false){
+                    #add a new array entry
+                        $error[] = 'This is not an PDF file, upload is allowed only for PDF files.';
+                    if($_FILES['upload']['size'] > 1000000){
+                         $error[]='The file exceeded the upload limit';
+                    }
+                    if(empty($error)){    
+                        move_uploaded_file($_FILES['upload']['tmp_name'], "uploads/{$_FILES['upload']['name']}");     
+                    }
+                }
+            }
+            if (!empty($error)){
+               foreach ($error as $err){
+                echo $err;
+                }
+            }
+        ?>
+            <form id="featured_form" method="post" action="" enctype="multipart/form-data">
+              Name:<br>
+              <input type="text" name="employee_name" class="back" value=""><br><br>
+              Surname:<br>
+              <input type="text" name="employee_surname" class="back" value=""><br><br>
+              Motivation letter (max 5000 characters):<br>
+              <textarea maxlength="5000" type="text" class="back" name="description" value="description" cols="40px" rows="15" wrap="soft"></textarea><br><br>
+              Upload your CV (pdf format):<br>
+              <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+              <input type="submit" value="Apply" name="submit_application" class="submit_forms"><br><br>
+             </form>
+             <?php 
+             
+            foreach ($emailsId as $key => $value) {
+                echo "<h1>".$value."</h2><br>";
+            }
+             ?>
         </div>
 	</div>
     </div>
